1e3477c4acb587701300fd79999e661b9a62884e
pdemetci <pinardemetci@gmail.com>
2015-02-28 21:42:26 -0500

    Arduino Code for the Stepper Motors

--- stepperShield.ino
+++ stepperShield.ino
@@ -1,41 +1,66 @@
+#include <Servo.h>
+
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_PWMServoDriver.h"
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 
-Adafruit_StepperMotor *motor1 = AFMS.getStepper(200, 1); // Define motor 1
-Adafruit_StepperMotor *motor2 = AFMS.getStepper(200, 2); // Define motor 2
+Adafruit_StepperMotor *motor1 = AFMS.getStepper(20, 1); // Define motor 1 getStepper(#steps per revolution, stepper#)
+Adafruit_StepperMotor *motor2 = AFMS.getStepper(20, 2); // Define motor 2
 
-int spd = 5;
-int steps = 10;
+int spd = 1000; //Revolution per minute
+int steps = 200; 
 int steptype = MICROSTEP;
+int a = 1;
+int i = 0;
 
 void setup(){
-  motor1.setSpeed(spd); // Set speeds of motors 1 and 2 (rotations per minute)
-  motor2.setSpeed(spd);
+  Serial.begin(9600);
+  AFMS.begin();
+  motor1->setSpeed(spd); // Set speeds of motors 1 and 2 (rotations per minute)
+  motor2->setSpeed(spd);
 }
 
 void loop(){
   if (a == 1){ //made up random variable to separate cases
     // Rotate right
-    motor1.step(steps, FORWARD, steptype); //step(#steps, direction, steptype)
-    motor2.step(steps, BACKWARD, steptype);
+    Serial.println("right");
+    for (int i = 0; i < steps; i++){
+      motor1->step(1, FORWARD, steptype); //step(#steps, direction, steptype)
+      motor2->step(1, BACKWARD, steptype);
+    }
+    a = 2;
   }
   else if (a == 2){
     // Rotate left
-    motor1.step(steps, BACKWARD, steptype);
-    motor2.step(steps, FORWARD, steptype);
+    Serial.println("left");
+    for (int i = 0; i < steps; i++){
+      motor1->step(1, BACKWARD, steptype);
+      motor2->step(1, FORWARD, steptype);
+    }
+    a = 3;
+
   }
   else if (a == 3){
     // Move forward
-    motor1.step(steps, FORWARD, steptype);
-    motor2.step(steps, FORWARD, steptype);
+    Serial.println("forward");
+    for (int i = 0; i < steps; i++){
+      motor1->step(1, FORWARD, steptype);
+      motor2->step(1, FORWARD, steptype);
+    }
+    a = 4;
+
   }
   else {
     // Move backward
-    motor1.step(steps, BACKWARD, steptype);
-    motor2.step(steps, BACKWARD, steptype);
+    Serial.println("backward");
+    for (int i = 0; i < steps; i++){
+      motor1->step(1, BACKWARD, steptype);
+      motor2->step(1, BACKWARD, steptype);
+    }
+    a = 1;
+
   }
   
 }
