60343f9faf6c5d2af5a1ca7e6b8afa95eb74cfd7
lvanderlyn <lindsey.vanderlyn@students.olin.edu>
2015-02-28 22:56:24 -0500

    Added string parsing, NOT debugged

--- stepperShield.ino
+++ stepperShield.ino
@@ -10,10 +10,11 @@ Adafruit_StepperMotor *motor1 = AFMS.getStepper(20, 1); // Define motor 1 getSte
 Adafruit_StepperMotor *motor2 = AFMS.getStepper(20, 2); // Define motor 2
 
 int spd = 1000; //Revolution per minute
-int steps = 200; 
 int steptype = MICROSTEP;
-int a = 1;
-int i = 0;
+int state = 0;
+String incomingString = '';
+String bufferString = '';
+
 
 void setup(){
   Serial.begin(9600);
@@ -22,45 +23,59 @@ void setup(){
   motor2->setSpeed(spd);
 }
 
-void loop(){
-  if (a == 1){ //made up random variable to separate cases
-    // Rotate right
-    Serial.println("right");
-    for (int i = 0; i < steps; i++){
+void right_turn(int steps){
+  for (int i = 0; i < steps; i++){
       motor1->step(1, FORWARD, steptype); //step(#steps, direction, steptype)
       motor2->step(1, BACKWARD, steptype);
     }
-    a = 2;
   }
-  else if (a == 2){
-    // Rotate left
-    Serial.println("left");
-    for (int i = 0; i < steps; i++){
+  
+void left_turn(int steps){
+  for (int i = 0; i < steps; i++){
       motor1->step(1, BACKWARD, steptype);
       motor2->step(1, FORWARD, steptype);
     }
-    a = 3;
 
-  }
-  else if (a == 3){
-    // Move forward
-    Serial.println("forward");
+void forward(int steps){
     for (int i = 0; i < steps; i++){
       motor1->step(1, FORWARD, steptype);
       motor2->step(1, FORWARD, steptype);
     }
-    a = 4;
-
-  }
-  else {
-    // Move backward
-    Serial.println("backward");
-    for (int i = 0; i < steps; i++){
-      motor1->step(1, BACKWARD, steptype);
-      motor2->step(1, BACKWARD, steptype);
-    }
-    a = 1;
-
-  }
   
+void loop(){
+  switch(state) {
+    // CASE 0 = Arduino reading mode
+    case 0: {
+      if (Serial.available() > 0) {
+      	// get one char from what Python sent
+        int incoming = Serial.read();
+
+        // was it end character?
+        if (char(incoming)=='/') {
+          state = 1; // switch to writing mode - DO NOT MODIFY
+        } else {
+          incomingString = incomingString + char(incoming); // add to string of notes
+        }
+      }
+      break;
+    }
+    case 1:{
+      char array = char[incomingString.length()];
+      incomingString.toCharArray(array, incomingString.length())
+      for(int i = 0; i < incomingString.length()*0.5; i+=2){
+        if (array[i] == 'f'){
+          forward(array[i+1]);
+        }
+        else if(array[i] == 'r'{
+          right_turn(array[i+1]);
+        }
+        else if(array[i] == 'l'{
+          left_turn(array[i+1]);
+        }
+        else{
+          
+        }
+      incomingStrign = '';
+      state = 0;
+ 
 }
